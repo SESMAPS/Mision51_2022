d73f382c83f04f3c046436510e976597699643cf
minombre dbrde <dvrg@example.com>
2022-05-03 22:16:24 -0500

    ejercicio para el dia 4Mayo

--- Ciclo1/Clase3_5EjercicioMañana.py
+++ Ciclo1/Clase3_5EjercicioMañana.py
@@ -6,7 +6,9 @@
 # version ='1.0'
 
 #Se trata de escribir el algoritmo que permita calcular el valor a pagar para una compra de un articulo determinado 
-#del que se adquieren una o varias unidades. El iva a aplicar es del 19% se trata de escsi el precio bruto (precio de venta mas IVA)
+#del que se adquieren una o varias unidades. El iva a aplicar es del 19% y si el precio bruto(precio de venta mas IVA)
 #es mayor de 500.000 COP, se aplicará un descuento del 6.5% sobre el total.
 #Se debe pedir al usuario que ingrese el valor del articulo y la cantidad.
 
+numero_articulos = int(input("Cantidad de articulos comprados: ")
+precio_articulo = 
