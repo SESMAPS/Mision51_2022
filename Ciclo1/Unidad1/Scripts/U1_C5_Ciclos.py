9748df03d458eedc3698d56245e40eda037d5a5f
minombre dbrde <dvrg@example.com>
2022-05-10 12:58:47 -0500

    numero primo

--- Ciclo1/Unidad1/Scripts/U1_C5_Ciclos.py
+++ Ciclo1/Unidad1/Scripts/U1_C5_Ciclos.py
@@ -7,12 +7,12 @@
 # Programa para escribir en pantalla los números de 1 al 10:
 
 x = 1
-while x <= 10: #Aqui definimos el limite hasta donde llegará
+while x <= 10: #Un while es un Mientras en psudocodigoAqui definimos el limite hasta donde llegará
     print(x)
     x = x + 1 #Cuando hacemos un while tenemos que asegurarnos que la variable de control siga aumentando, para poder que acabe en algun momento
 
 y = 1
-while y <= 10: #
+while y <= 10: # Generalmente tenemos una variable de control. Pero no siempre estará
     print(y)
     y = y + 2 #Imprime numeros pares
 
@@ -22,3 +22,8 @@ u = 1
 while z <= 90: 
     print(f"Multiplicar 9 por {u + 1} da como resultado: {z}") # si uso + para concatenar ejemplo + str(z) iria despues de ""final, si está dentro de comilla es {}
     z = z + 9
+
+a = 1
+while a <= 90: # Si se hace así funciona diferente. 
+    a = a + 9
+    print(a)
--- Ciclo1/Unidad1/Scripts/U1_C6.py
+++ Ciclo1/Unidad1/Scripts/U1_C6.py
@@ -2,6 +2,42 @@
 # -*- coding: utf-8 -*- 
 #----------------------------------------------------------------------------
 # Creado por  : davoc
-# En : Clase 6 Ciclos ciclo 1 mision tic 2022. 9 Mayo 2022 
+# En : Clase 6 Ciclos, ejemplos ciclo 1 mision tic 2022. 9 Mayo 2022 
 # # version ='1.0' VsCodium
 
+#Si quiero escribir un programa que pregunte al usuario un número y le diga si es un primo o no
+
+numero = int(input("Ingrese un número: "))
+canditad_divisores = 0
+
+x = 2
+while x < numero:  #porque es menor al anterior que la persona, no justo en el limite
+    if numero % x == 0:
+        canditad_divisores = canditad_divisores + 1
+    x = x +1
+if canditad_divisores >0 or numero < 2:# o < 1El or lo uso por la consideración especial
+    print("Bro el número no es primo")
+else:
+    print("Bro el número es primo")
+
+
+#Si quiero escribir un programa que pregunte sucesivamente al usuario un número mayor que cero y le diga si es un primo o no
+#Si el usuario digita 0, el programa debe terminar
+#https://www.youtube.com/watch?v=HiXLkL42tMU&t=1064s
+
+numero = 1
+while numero != 0:
+    numero = int(input("Ingrese un número (para temrinar ingrese 0): ")) #lo ponemos aqui porque queremos que se repita esta pregunta, por eso no esta fuera del while
+    if numero ==0:
+        print("Fin del programa")
+    else:
+        canditad_divisores = 0
+        x = 2
+        while x < numero:  #porque es menor al anterior que la persona, no justo en el limite
+            if numero % x == 0:
+                canditad_divisores = canditad_divisores + 1
+            x = x +1
+        if canditad_divisores >0 or numero < 2:# o < 1El or lo uso por la consideración especial
+            print("Bro el número no es primo")
+        else:
+            print("Bro el número es primo")
