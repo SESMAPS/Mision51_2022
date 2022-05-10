f523ee0965de49eb617aa1000f65668e19e25d52
minombre dbrde <dvrg@example.com>
2022-05-10 12:01:11 -0500

    clase ciclos

---
+++ Ciclo1/Unidad1/Scripts/U1_C5_Ciclos.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3  
+# -*- coding: utf-8 -*- 
+#----------------------------------------------------------------------------
+# Creado por  : davoc
+# En : Clase 5 Ciclos ciclo 1 mision tic 2022. 9 Mayo 2022 
+# # version ='1.0' VsCodium
+# Programa para escribir en pantalla los números de 1 al 10:
+
+x = 1
+while x <= 10: #Aqui definimos el limite hasta donde llegará
+    print(x)
+    x = x + 1 #Cuando hacemos un while tenemos que asegurarnos que la variable de control siga aumentando, para poder que acabe en algun momento
+
+y = 1
+while y <= 10: #
+    print(y)
+    y = y + 2 #Imprime numeros pares
+
+z = 9
+u = 1
+
+while z <= 90: 
+    print(f"Multiplicar 9 por {u + 1} da como resultado: {z}") # si uso + para concatenar ejemplo + str(z) iria despues de ""final, si está dentro de comilla es {}
+    z = z + 9
---
+++ Ciclo1/Unidad1/Scripts/U1_C6.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3  
+# -*- coding: utf-8 -*- 
+#----------------------------------------------------------------------------
+# Creado por  : davoc
+# En : Clase 6 Ciclos ciclo 1 mision tic 2022. 9 Mayo 2022 
+# # version ='1.0' VsCodium
+
