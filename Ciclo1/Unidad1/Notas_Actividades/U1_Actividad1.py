7646f803ebdbde7b6e1694de5ef51094c406a12f
minombre dbrde <dvrg@example.com>
2022-05-11 12:02:40 -0500

    Agrega docstring a la función

--- Ciclo1/Unidad1/Notas_Actividades/U1_Actividad1.py
+++ Ciclo1/Unidad1/Notas_Actividades/U1_Actividad1.py
@@ -7,6 +7,19 @@
 #Escribir un algoritmo que lea cuatro números e indique cual es el mayor
 
 def mayor(num1:float,num2:float,num3:float,num4:float):
+    """La función mayor admite como parametros de entrada 4 números reales, para luego realizar 
+    la consulta con la funcion max, con el fin de devolver el número mayor que se usaron como parametros
+    de entrada.
+
+    Args:
+        num1 (float): _description_
+        num2 (float): _description_
+        num3 (float): _description_
+        num4 (float): _description_
+
+    Returns:
+        _Cadena de texto_: Esta cadena le dice al usuario cual es el número mayor
+    """
     consultar = (max(num1, num2, num3, num4))
     return f"El número mayor de los 4 es: {consultar}"
 
