ebd9d475af60167ce9568874db1ba684863f3fcc
minombre dbrde <dvrg@example.com>
2022-05-16 17:37:32 -0500

    Soluciona la opcion 2, con metodo get

--- Ciclo1/Unidad1/Scripts/U1_C7_ejemploAgenda.py
+++ Ciclo1/Unidad1/Scripts/U1_C7_ejemploAgenda.py
@@ -14,30 +14,34 @@
 # Opcion 2 , un diccionario dentro de otro dicc
 # {1:{'Nombre':'Juan', 'Telefono':123}, 2:{'Nombre':'Pedro', 'Telefono':6544}}
 
-#el .key comprueba dentro de un dict si existe el valor que yo le estoy pasando
+#el .key comprueba dentro de un dict si existe el valor que yo le estoy pasando Pero hay que poner mucho cuidado como se quiere consultar
+#En algun caso es mejor usar el get.
 
 agenda = {}
-opcion = int(input(""""Elija una opcion:
-                    1. Agregar un contacto
-                    2. Buscar un contacto
-                    3. Eliminar un contacto
-                    4. Ver todos los contactos guardados                             
-                """))
-if opcion == 1: #El usuario quiere agregar un contacto
-    nombre = input("Ingrese el nombre del nuevo contacto: ")
-    agenda[nombre] = input(f"Ingrese el teléfono del contacto de {nombre}: ")
-elif opcion == 2:
-    nombre = input("Ingrese el nombre del contacto que desea buscar")
-    if agenda.keys(nombre) != None:
-        print(f"{nombre} : {agenda[nombre]}")
-    else:
-        print("No existe un contacto con ese nombre")
-elif opcion == 3:
-    nombre = input("Ingrese el nombre del contacto que desee eliminar: ")
-    if agenda.keys(nombre) != None:
-        del agenta[nombre]
-    else:
-        print("No existe un contacto con ese nombre !")
-elif opcion == 4:
-    for key in agenda:
-        print(f"{key} : {agenda[key]}")
+opcion = 1
+while opcion != 5:
+    opcion = int(input(""""Elija una opcion:
+                        1. Agregar un contacto
+                        2. Buscar un contacto
+                        3. Eliminar un contacto
+                        4. Ver todos los contactos guardados  
+                        5. Salir                           
+                    """))
+    if opcion == 1: #El usuario quiere agregar un contacto
+        nombre = input("Ingrese el nombre del nuevo contacto: ")
+        agenda[nombre] = input(f"Ingrese el teléfono del contacto de {nombre}: ")
+    elif opcion == 2:
+        nombre = input("Ingrese el nombre del contacto que des1ea buscar: ")
+        if agenda.get(nombre) != None: #FIXME: se arregló la consulta, el metodo es get en lugar de key, o fromkeys-
+            print(f"{nombre} : {agenda[nombre]}")
+        else:
+            print("No existe un contacto con ese nombre")
+    elif opcion == 3:
+        nombre = input("Ingrese el nombre del contacto que1 desee eliminar: ")
+        if agenda.fromkeys(nombre) != None:
+            del agenta[nombre]
+        else:
+            print("No existe un contacto con ese nombre !")
+    elif opcion == 4:
+        for key in agenda:
+            print(f"{key} : {agenda[key]}")
