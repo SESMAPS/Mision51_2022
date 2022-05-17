a351cd7062e4e19d2c81c88839c5bdd86af89387
minombre dbrde <dvrg@example.com>
2022-05-17 12:14:25 -0500

    Clase9_Calculador de notas

---
+++ Ciclo1/Unidad1/Scripts/U1_Clase8_C9.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3  
+# -*- coding: utf-8 -*- 
+#----------------------------------------------------------------------------
+# Creado por  : davoc
+# Tema: Clase 8 no se escribio codigo. Clase 9. Parte 1. 
+# Para: mision tic 2022. 16 Mayo 2022 
+# # version ='1.0' VsCodium
+
+""" programa que le pregunte al usuarii (profesor) el nombre del alumno, el numero de notas
+que desea ingresar y los porcentajes correspondientes a cada una de ellas.
+Posteriormente, de acuerdo con el numero de notas deseado, el programa debe preguntar al usuario el valor de cada una
+de esas notas, para finalmente desplegar en pantalla el informe completo de notas del estuciante,
+así (teniendo en cuenta que aprueba el curso con una nota mayor o igual a 3.0):
+Nombre del estudiante: xxxx
+Nota No. n: X.X
+.
+.
+.
+Nota final: X.X
+Aprueba: Si/No
+Ej: Si son 2 notas: {'nombre': 'Juanito',0: 3.5, 'porcentaje_0':50,1: 4.0, 'porcentaje_1':50} """
+
--- Ciclo1/Unidad1/Scripts/U1_Clase8_p1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3  
-# -*- coding: utf-8 -*- 
-#----------------------------------------------------------------------------
-# Creado por  : davoc
-# Tema: Clase 8 Parte 1. 
-# Para: mision tic 2022. 16 Mayo 2022 
-# # version ='1.0' VsCodium
-
