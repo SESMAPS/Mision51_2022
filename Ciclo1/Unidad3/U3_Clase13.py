b09ca5e388e546b330370596e7bb3359f6232a77
minombre dbrde <dvrg@example.com>
2022-05-25 12:38:06 -0500

    operadores python

--- Ciclo1/Unidad3/U3_Clase13.py
+++ Ciclo1/Unidad3/U3_Clase13.py
@@ -2,10 +2,16 @@
 # -*- coding: utf-8 -*- 
 #----------------------------------------------------------------------------
 # Creado por  : davoc
-# Tema: Clase 13 
-# . 
+# Tema: Clase 13  Herramientas del lenguaje
 # Para: mision tic 2022. 25 Mayo 2022 
 # # version ='1.0' VsCodium
 #Operadores especiales para simplificar la iteracion dentro de objetos en lugar de recorrerlos con ciclos for
+
+#Operador Lambda - definir unas funciones "desechables" de una forma rapida indicar que hacer con ese 
+#conjunto de datos . Pero lambda debe ser para funciones cortas, una sola instrucción
+#ejemplo: 
+
+def suma(x,y):
+    return x+y
 
 
