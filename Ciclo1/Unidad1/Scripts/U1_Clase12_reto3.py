c1a0533a57ea653dac2981b844f6c939acca72be
minombre dbrde <dvrg@example.com>
2022-05-25 12:32:42 -0500

    Actualiza Clases

---
+++ Ciclo1/Unidad1/Scripts/U1_Clase12_reto3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3  
+# -*- coding: utf-8 -*- 
+#----------------------------------------------------------------------------
+# Creado por  : davoc
+# Tema: Clase 12 Estructura para el reto3
+# . 
+# Para: mision tic 2022. 24 Mayo 2022 
+# # version ='1.0' VsCodium
---
+++ Ciclo1/Unidad3/U3_Clase13.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3  
+# -*- coding: utf-8 -*- 
+#----------------------------------------------------------------------------
+# Creado por  : davoc
+# Tema: Clase 13 
+# . 
+# Para: mision tic 2022. 25 Mayo 2022 
+# # version ='1.0' VsCodium
+#Operadores especiales para simplificar la iteracion dentro de objetos en lugar de recorrerlos con ciclos for
+
+
